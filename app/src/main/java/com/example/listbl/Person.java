08ef0ef295cb121184e83c125f4260d9a4fd7afe
DenisVikoruk <dd.vikoruk@gmail.com>
2024-02-13 22:28:49 +0300

    хачапури

---
+++ app/src/main/java/com/example/listbl/Person.java
@@ -0,0 +1,12 @@
+package com.example.listbl;
+
+public class Person {
+    private int id;
+    private String name;
+    private String phone;
+    public void Person(int id, String name, String phone){
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+}
